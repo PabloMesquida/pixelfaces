1291101c7f3bfe62f2a21a026d6e147c13ae6884
PabloMesquida <pablomesquida67@gmail.com>
2022-09-30 18:49:35 +0200

    isMobile

--- src/components/main/Desktop.js
+++ src/components/main/Desktop.js
@@ -1,46 +1,11 @@
 import React, { useRef, Suspense } from "react";
-import { useFrame } from "@react-three/fiber";
+
 import { Stars, OrbitControls } from "@react-three/drei";
-import Earth from "./Earth.js";
-import Moon from "./Moon.js";
 
 const Desktop = () => {
-  const refEarth = useRef();
-  const refMoon = useRef();
-
-  function Update() {
-    useFrame(({ clock, pointer, camera }) => {
-      refEarth.current.rotation.y += -0.0015;
-      refEarth.current.rotation.z += -0.0001;
-
-      let elapsed = clock.getElapsedTime();
-
-      refMoon.current.position.x = Math.sin(elapsed * 0.1) * 2.5;
-      refMoon.current.position.y = Math.abs(Math.cos(elapsed * 0.01));
-      refMoon.current.position.z = Math.cos(elapsed * 0.1) * 2.5;
-      refMoon.current.lookAt(refEarth.current.position);
-
-      const parallaxX = pointer.x * 1;
-      const parallaxY = -pointer.y * 1;
-
-      camera.position.x += (parallaxX - camera.position.x) * 0.2;
-      camera.position.y += (parallaxY - camera.position.y) * 0.2;
-
-      camera.lookAt(0, 0, 0);
-    });
-  }
   return (
     <>
       <Stars count={1000} radius={20} />
-      <ambientLight intensity={5} />
-      <Suspense>
-        <group ref={refEarth}>
-          <Earth rotation={[Math.PI * 1.1, 0, Math.PI * 0.2]} />
-        </group>
-        <group ref={refMoon}>
-          <Moon scale={[0.25, 0.25, 0.25]} />
-        </group>
-      </Suspense>
     </>
   );
 };
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,14 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { DeviceOrientationControls } from "@react-three/drei";
+
 import styled from "styled-components";
 
 import Desktop from "./Desktop.js";
 import Mobile from "./Mobile.js";
 import PortalView from "./PortalView.js";
+import Earth from "./Earth.js";
+import Moon from "./Moon.js";
 
 let vh = window.innerHeight;
 
@@ -18,6 +22,31 @@ const MainAnimation = styled.section`
 const Main = () => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+  const refEarth = useRef();
+  const refMoon = useRef();
+
+  function Update() {
+    useFrame(({ clock, pointer, camera }) => {
+      refEarth.current.rotation.y += -0.0015;
+      refEarth.current.rotation.z += -0.0001;
+
+      let elapsed = clock.getElapsedTime();
+
+      refMoon.current.position.x = Math.sin(elapsed * 0.1) * 2.5;
+      refMoon.current.position.y = Math.abs(Math.cos(elapsed * 0.01));
+      refMoon.current.position.z = Math.cos(elapsed * 0.1) * 2.5;
+      refMoon.current.lookAt(refEarth.current.position);
+
+      const parallaxX = pointer.x * 1;
+      const parallaxY = -pointer.y * 1;
+
+      camera.position.x += (parallaxX - camera.position.x) * 0.2;
+      camera.position.y += (parallaxY - camera.position.y) * 0.2;
+
+      camera.lookAt(0, 0, 0);
+    });
+  }
+
   return (
     <MainAnimation>
       <Canvas
@@ -25,7 +54,16 @@ const Main = () => {
         dpr={[1, 2]}
         camera={{ fov: 50, position: [0, 0, 10], near: 0.01, far: 3000 }}
       >
-        {isMobile ? <Mobile /> : <Desktop />}
+        <ambientLight intensity={5} />
+        <Suspense>
+          <group ref={refEarth}>
+            <Earth rotation={[Math.PI * 1.1, 0, Math.PI * 0.2]} />
+          </group>
+          <group ref={refMoon}>
+            <Moon scale={[0.25, 0.25, 0.25]} />
+          </group>
+        </Suspense>
+        {isMobile && <DeviceOrientationControls />}
       </Canvas>
     </MainAnimation>
   );
